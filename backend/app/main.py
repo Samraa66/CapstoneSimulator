6d4896e3b8219c2d0aa5cd04d5afb1a19a3b6209
Karim Gorgy <karimramzy2507@gmail.com>
2025-01-28 17:12:59 -0500

    Added trains to map

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from app.routes import router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(title="Transportation Emissions API")
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Allow specific origins (your frontend URL)
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 # Include API routes
 app.include_router(router)
 
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -5,12 +5,13 @@ router = APIRouter()
 
 # Load datasets (use actual paths to your CSV files)
 FLIGHTS_DATA = "app/data/flights.csv"
-TRAINS_DATA = "app/data/trains.csv"
+TRAINS_DATA = "app/data/train_unique_routes.csv"
 
 # Helper function to load data
 def load_data(file_path):
     return pd.read_csv(file_path)
-
+#trains = load_data(TRAINS_DATA)
+#flights = load_data(FLIGHTS_DATA)
 # Endpoint: Fetch all flight data
 @router.get("/api/flights")
 def get_flights():
@@ -21,8 +22,14 @@ def get_flights():
 @router.get("/api/trains")
 def get_trains():
     trains = load_data(TRAINS_DATA)
+    trains = trains[(trains['DestLat'].notnull()) & (trains['DestLon'].notnull()) & (trains['OriginLat'] != "") & (trains['OriginLon'] != "")]
+    trains = trains.fillna(value="1.0")
+    print(f"train dataset length {len(trains)}")
+    
     return trains.to_dict(orient="records")
-
+@router.get("/api/trainEmissions")
+def get_train_emissions():
+    trains['emissions']
 # Endpoint: Filter flight data by origin and destination
 @router.get("/api/flights/search")
 def search_flights(origin: str, destination: str):
